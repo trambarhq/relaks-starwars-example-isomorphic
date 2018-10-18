51c265d7b1946e7fca26044056c445f8f965afff
Chung Leong <chernyshevsky@hotmail.com>
2018-10-18 14:55:54 +0200

    Removed params from load().

--- src/routing.js
+++ src/routing.js
@@ -29,85 +29,85 @@ class Route {
 let routes = {
     'welcome': {
         path: '/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/welcome-page' /* webpackChunkName: "welcome-page" */);
         }
     },
     'film-list': {
         path: '/films/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/film-list' /* webpackChunkName: "film-list" */);
         }
     },
     'film-summary': {
         path: '/films/${id}/',
         params: { id: Number },
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/film-page' /* webpackChunkName: "film-page" */);
         }
     },
     'character-list': {
         path: '/characters/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/character-list' /* webpackChunkName: "character-list" */);
         }
     },
     'character-summary': {
         path: '/characters/${id}/',
         params: { id: Number },
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/character-page' /* webpackChunkName: "character-page" */);
         }
     },
     'planet-list': {
         path: '/planets/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/planet-list' /* webpackChunkName: "planet-list" */);
         }
     },
     'planet-summary': {
         path: '/planets/${id}/',
         params: { id: Number },
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/planet-page' /* webpackChunkName: "planet-page" */);
         }
     },
     'species-list': {
         path: '/species/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/species-list' /* webpackChunkName: "species-list" */);
         }
     },
     'species-summary': {
         path: '/species/${id}/',
         params: { id: Number },
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/species-page' /* webpackChunkName: "species-page" */);
         }
     },
     'vehicle-list': {
         path: '/vehicles/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/vehicle-list' /* webpackChunkName: "vehicle-list" */);
         }
     },
     'vehicle-summary': {
         path: '/vehicles/${id}/',
         params: { id: Number },
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/vehicle-page' /* webpackChunkName: "vehicle-page" */);
         }
     },
     'starship-list': {
         path: '/starships/',
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/starship-list' /* webpackChunkName: "starship-list" */);
         }
     },
     'starship-summary': {
         path: '/starship/${id}/',
         params: { id: Number },
-        load: async (params) => {
+        load: async () => {
             params.module = await import('pages/starship-page' /* webpackChunkName: "starship-page" */);
         }
     },
