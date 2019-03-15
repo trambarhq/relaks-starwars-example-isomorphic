90e34a5fd36078c3fca8ac62176c76af88c6d9db
Chung Leong <chernyshevsky@hotmail.com>
2019-03-15 01:28:05 -0400

    Upgraded to WebPack 4.
    Upgraded other packages.

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,14 @@ var FS = require('fs');
 var Path = require('path');
 var Webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var DefinePlugin = Webpack.DefinePlugin;
 var NamedChunksPlugin = Webpack.NamedChunksPlugin;
-var NamedModulesPlugin = Webpack.NamedModulesPlugin;
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 var event = process.env.npm_lifecycle_event;
 
 var clientConfig = {
+    mode: (event === 'build') ? 'production' : 'development',
     context: Path.resolve('./src'),
     entry: './main',
     output: {
@@ -45,23 +43,26 @@ var clientConfig = {
                 }
             },
             {
+                test: /\.css$/,
+                use: [ MiniCSSExtractPlugin.loader, 'css-loader' ],
+            },
+            {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader!sass-loader',
-                })
+                use: [ MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader' ],
             },
         ]
     },
     plugins: [
         new NamedChunksPlugin,
-        new NamedModulesPlugin,
         new BundleAnalyzerPlugin({
             analyzerMode: (event === 'build') ? 'static' : 'disabled',
             reportFilename: `report.html`,
         }),
-        new ExtractTextPlugin("styles.css"),
+        new MiniCSSExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
     ],
-    devtool: (event === 'build') ? false : 'inline-source-map',
 };
 
 var serverConfig = {
@@ -73,46 +74,25 @@ var serverConfig = {
         filename: 'front-end.js',
         chunkFilename: '[name].js',
         libraryTarget: 'commonjs2',
-        publicPath: '/starwars',
+        publicPath: '/starwars/',
     },
     resolve: clientConfig.resolve,
     module: clientConfig.module,
     plugins: [
         new NamedChunksPlugin,
-        new NamedModulesPlugin,
         new HtmlWebpackPlugin({
             template: Path.resolve(`./src/index.html`),
             filename: Path.resolve(`./server/client/index.html`),
         }),
-        new ExtractTextPlugin('styles.css'),
+        new MiniCSSExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
     ],
-    devtool: clientConfig.devtool,
 };
 
 var configs = module.exports = [ serverConfig, clientConfig ];
 
-var constants = {};
-if (event === 'build') {
-    console.log('Optimizing JS code');
-
-    configs.forEach((config) => {
-        // set NODE_ENV to production
-        var plugins = config.plugins;
-        var constants = {
-            'process.env.NODE_ENV': '"production"',
-        };
-        plugins.unshift(new DefinePlugin(constants));
-
-        // use Uglify to remove dead-code
-        plugins.unshift(new UglifyJSPlugin({
-            uglifyOptions: {
-                compress: {
-                  drop_console: true,
-                }
-            }
-        }));
-    })
-}
 
 // copy webpack.resolve.js into webpack.debug.js to resolve Babel presets
 // and plugins to absolute paths, required when linked modules are used
